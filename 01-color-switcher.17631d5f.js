124d30b3b49d9889fe4293b52c14a58756339f45
LexMorrison <119893721+LexMorrison@users.noreply.github.com>
2023-05-21 23:16:15 +0000

    Deploying to gh-pages from @ LexMorrison/goit-js-hw-09@ebc6e20caa736924579f2b279d100194ad098a66 🚀

---
+++ 01-color-switcher.17631d5f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),a=null;t.addEventListener("click",(function(d){a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(r){clearInterval(a),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)})),e.setAttribute("disabled",!0)}();
+//# sourceMappingURL=01-color-switcher.17631d5f.js.map
